e5224e13918afd99b28ace437690b8ec7478627e
gunjansoral <gunjan.soral@gmail.com>
2025-04-19 11:15:21 +0530

    test deployment

--- archonist-web-frontend/src/App.tsx
+++ archonist-web-frontend/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
       <h1>Archonist Official Site </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          Coming Soon {count}
+          Coming Soon
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
