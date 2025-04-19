cbb64ce704b01fd48d55ecff03dbcc7b226b5076
gunjansoral <gunjan.soral@gmail.com>
2025-04-19 11:28:24 +0530

    test deployment

--- archonist-web-frontend/src/App.tsx
+++ archonist-web-frontend/src/App.tsx
@@ -19,9 +19,7 @@ function App() {
         </button>
         
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
